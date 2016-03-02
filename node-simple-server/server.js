fcd0c35e24d602a3d0047a2f787fb5a7bc4d9b07
Paul Bouzakis <github@paulbouzakis.com>
2016-03-02 15:49:06 -0500

    Add a simple server to handle student posts.

---
+++ node-simple-server/server.js
@@ -0,0 +1,40 @@
+var http = require("http");
+var querystring = require("querystring");
+
+var server = http.createServer(function(req, res) {
+    console.log(req.method + " to " + req.url);
+    if (req.method === "POST" && req.url === "/students") {
+        handlePostStudents(req, res);
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+function handlePostStudents(req, res) {
+    var fullBody = "";
+
+    req.on("data", function(chunk) {
+        // append the current chunk of data to the fullBody variable
+        fullBody += chunk.toString();
+    });
+
+    req.on("end", function() {
+        // request ended -> do something with the data
+        res.writeHead(200, "OK", {"Content-Type": "text/html"});
+
+        // parse the received body data
+        var studentInfo = querystring.parse(fullBody);
+        // console.dir(decodedBody);
+
+        // output the decoded data to the HTTP response
+        res.write("<html><head><title>Post data</title></head><body><pre>");
+        res.write(JSON.stringify(studentInfo));
+        res.write("</pre></body></html>");
+
+        res.end();
+    });
+}
+
+server.listen(8080);
+console.log("Listening on port 8080.");

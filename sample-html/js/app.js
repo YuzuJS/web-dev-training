b28fc5a9c2146ab5141d7a4c7fe74db8bc4510e9
Paul Bouzakis <github@paulbouzakis.com>
2016-03-02 15:28:01 -0500

    Error display and range output.

--- sample-html/js/app.js
+++ sample-html/js/app.js
@@ -4,21 +4,38 @@ var APP = {};
 (function (exports) {
 
 function ContactController(el) {
-    this._formEl = el.querySelector(".student-form");
+    this._el = el;
+    this._formEl = this._query(".student-form");
     this._formEl.addEventListener("submit", this._handleFormSubmit.bind(this));
+    this._ageInput = this._query("#age");
+    this._ageOutput = this._query("#age-output");
+    this._ageInput.addEventListener("input", this._handleAgeChange.bind(this));
 }
 
 ContactController.prototype._handleFormSubmit = function (ev) {
     // The form is valid as long as user enters email.
-    var emailInput = this._formEl.querySelector("#email");
-    var isValid = !!emailInput.value;
+    var pwdInput = this._query("#pwd");
+    var pwd = pwdInput.value;
+    var confirm = this._query("#confirm-pwd").value;
 
-    if (!isValid) {
-        alert("Bad Input!");
+    if (pwd !== confirm) {
         ev.preventDefault();
-        emailInput.focus();
+        this._showErrorMessage();
+        pwdInput.focus();
     }
+};
 
+ContactController.prototype._handleAgeChange = function (ev) {
+    this._ageOutput.textContent = "(" + this._ageInput.value + ")";
+};
+
+ContactController.prototype._query = function (selector) {
+    return this._el.querySelector(selector);
+};
+
+ContactController.prototype._showErrorMessage = function () {
+    var msgEl = this._query("#error-message");
+    msgEl.classList.remove("hidden");
 };
 
 exports.ContactController = ContactController;

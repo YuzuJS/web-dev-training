316bf30132910706fdc3ad29e1f56a44801968d5
zjordan <zjordan@book.com>
2016-03-04 11:00:24 -0500

    Modified server to use studentContactContrller.

--- node-simple-server/server.js
+++ node-simple-server/server.js
@@ -1,43 +1,17 @@
 var http = require("http");
-var querystring = require("querystring");
 var staticFiles = require("./staticFileController");
+var studentContacts = require("./studentContactsController");
 
 var server = http.createServer(function(req, res) {
     console.log(req.method + " to " + req.url);
 
     if (req.method === "POST" && req.url === "/students") {
-        handlePostStudents(req, res);
-
+        studentContacts.handleRequest(req, res);
     } else {
         staticFiles.handleRequest(req, res);
     }
 });
 
-function handlePostStudents(req, res) {
-    var fullBody = "";
-
-    req.on("data", function(chunk) {
-        // append the current chunk of data to the fullBody variable
-        fullBody += chunk.toString();
-    });
-
-    req.on("end", function() {
-        // request ended -> do something with the data
-        res.setHeader("Location", "student-added.html");
-        res.writeHead(303, "OK", { "Content-Type": "text/html" });
-
-        // parse the received body data
-        var studentInfo = querystring.parse(fullBody);
-        // console.dir(decodedBody);
-
-        // output the decoded data to the HTTP response
-        res.write("<html><head><title>Post data</title></head><body><pre>");
-        res.write(JSON.stringify(studentInfo));
-        res.write("</pre></body></html>");
-
-        res.end();
-    });
-}
-
+studentContacts.init();
 server.listen(8080);
 console.log("Listening on port 8080.");

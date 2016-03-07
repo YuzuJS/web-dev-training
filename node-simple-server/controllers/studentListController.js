5c03d564ee5501e7b9cf9a7320cb4d368d1849ac
Gianluca Macciocca <gmaccioccca@book.com>
2016-03-07 13:13:05 -0500

    Render student HTML from controller.

--- node-simple-server/controllers/studentListController.js
+++ node-simple-server/controllers/studentListController.js
@@ -1,17 +1,29 @@
 var View = require("../View");
 var studentsView;
+var studentView;
 
 exports.init = function () {
     studentsView = new View("students");
+    studentView = new View("student");
 };
 
 exports.canAccept = function (req) {
     return req.method === "GET" && req.url === "/students";
 };
 
+function createStudentsHtml(students) {
+    var output = "";
+
+    Object.keys(students).forEach(function (id) {
+        output += studentView.render(students[id]);
+    });
+
+    return output;
+}
+
 exports.handleRequest = function (req, res) {
     var data = {
-        students: JSON.stringify(require("../data/studentContacts.json")),
+        students: createStudentsHtml(require("../data/studentContacts.json")),
     };
 
     var content = studentsView.render(data);
